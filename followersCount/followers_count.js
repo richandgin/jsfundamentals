366bfeece6423226ddcc17c0c67ce532be7d1d3f
Superman <superman@gmail.com>
2025-02-02 16:24:57 -0500

    added reset button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,9 @@ function checkCountValue() {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
   }
+
+function resetCount() {
+    count = 0; // Reset the count to zero
+    displayCount();  //Check and display the count value
+    alert("Your Follower's Count has been reset");
+}
