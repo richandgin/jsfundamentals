1ff8289e49889dbc2f846a690dadbc42cd98eedf
Superman <superman@gmail.com>
2025-02-02 17:13:53 -0500

    Added User Experience to HTML and JS

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -9,6 +9,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experienceText').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -17,6 +18,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
 
     //Make this block area of the HTML visible
     document.getElementById('userInfo').style.display = 'block';
