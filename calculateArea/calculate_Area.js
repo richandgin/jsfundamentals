5d134f1d61a277438a9b629e4edb182981470ac6
Superman <superman@gmail.com>
2025-02-02 14:19:24 -0500

    Corrections to Calc Areea

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -3,9 +3,13 @@ let width;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementByID('width').value);
+    width = parseFloat(document.getElementById('width').value);
     
     let area = length * width;
     //document.getElementById('result').innerText = 'The area of the rectangle is : ${area}';
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+    
+    console.log("Length: ", length);
+    console.log("Width: ", width);
+    console.log("Area: ", area);
 }
